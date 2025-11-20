287123a8210026b952e9b9c5422444c6bfde5c3e
Rimma Chukhno <rimma.chukhno@laplateforme.io>
2025-11-20 13:27:48 +0100

    add modified script

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,6 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewEmptyCSharpScript
+public class MenuController : MonoBehaviour
 {
-    
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void Continue()
+    {
+        string lastScene = PlayerPrefs.GetString("LastScene", "MainMenu");
+        SceneManager.LoadScene(lastScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
