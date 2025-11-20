5454297a7bcee8a1d74ed9dcfb5eab703b2a5f8f
Fathima Ouahhoud <fathima.ouahhoud@laplateforme.io>
2025-11-20 14:35:47 +0100

    add files into gitignore that were not present in the first creation

---
+++ Assets/Scripts/maps.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class maps : MonoBehaviour
+
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public float speedMultiplier = 1f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic; 
+        rb.gravityScale = 2f; 
+    }
+
+    void Update()
+    {
+        
+        rb.linearVelocity = new Vector2(speed * speedMultiplier, rb.linearVelocity.y);
+
+        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+            //GetComponent<RobotCollision>().PLayJumpSound();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
